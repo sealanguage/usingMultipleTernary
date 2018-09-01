afd908cd01a40c8351874590bce8fa8c503d9175
Elaine Froehlich <elaine.froehlich@gmail.com>
2018-09-01 03:34:26 +0000

    initial commit

---
+++ usingMultipleTernary.js
@@ -0,0 +1,11 @@
+function checkSign(num) {
+    
+    return (num > 0) ? "positive" : (num < 0) ? "negative" : "zero";
+  
+}
+
+checkSign(10);
+
+// 0 returns zero
+// positive numbers return positive
+// negative numbers return negative
